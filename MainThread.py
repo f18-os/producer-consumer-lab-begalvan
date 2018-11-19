941d18703da565d285e7de8ae1666fb9a0dd7e19
begalvan <begalvan@miners.utep.edu>
2018-11-19 00:43:41 -0700

    consumer class

--- MainThread.py
+++ MainThread.py
@@ -3,7 +3,6 @@
 import time
 import random
 from threading import Thread, Condition
-
 from ExtractFrames import *
 from ConvertToGrayscale import *
 from DisplayFrames import *
@@ -12,6 +11,7 @@ queue_list = [] #empty queue
 MAX_BOUND = 10  #max frames
 condition = Condition()
 
+#producer class
 class ProducerThread(Thread):
     def run(self):
         global queue_list
@@ -28,3 +28,14 @@ class ProducerThread(Thread):
             ExtractingThread().start()
             GrayscaleConvertThread().start()
             DisplayThread().start()
+
+            queue_list.append(count)
+            count = count + 1
+            condition.notify()
+            condition.release()
+            time.sleep(random.random())
+
+#consumer class
+class ConsumerThread(Thread):
+    def run(self):
+        global queue_list
