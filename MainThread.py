8fe96e06f554f8bfcd0da3129838d5ed87707ab4
begalvan <begalvan@miners.utep.edu>
2018-11-19 00:35:16 -0700

    rev main file

--- MainThread.py
+++ MainThread.py
@@ -3,3 +3,11 @@
 import time
 import random
 from threading import Thread, Condition
+
+from ExtractFrames import *
+from ConvertToGrayscale import *
+from DisplayFrames import *
+
+queue_list = [] #empty queue
+MAX_BOUND = 10  #max frames
+condition = Condition()
